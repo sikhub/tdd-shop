282a0321d44789038d8cc4d1b0beb4a6d4dff0d1
Blaz Cigale <blaz.cigale@gmail.com>
2017-03-23 20:35:39 +0000

    Typo in controller name

--- app/Http/Controllers/ConcertsController.php
+++ app/Http/Controllers/ConcertsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ConcerstController extends Controller
+class ConcertsController extends Controller
 {
     public function show($id)
     {
